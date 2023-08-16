dcf9258896b3d0e32cadd5f69d9ebcf479011f05
LaZy1220 <4elove4iska@gmail.com>
2023-08-16 11:37:01 +0300

    complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const openingBrackets = bracketsConfig.map((pair) => pair[0]);
+  const closingBrackets = bracketsConfig.map((pair) => pair[1]);
+  const matchingBrackets = Object.fromEntries(bracketsConfig);
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (openingBrackets.includes(char)) {
+      stack.push(char);
+    } else if (closingBrackets.includes(char)) {
+      if (stack.length === 0) {
+        return false;
+      }
+      const top = stack.pop();
+      if (matchingBrackets[top] !== char) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
